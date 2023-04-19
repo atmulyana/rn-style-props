61c3d1c232d9cc55ebe3f85d9926fecdeb2ac4b5
AT Mulyana <atmulyana@yahoo.com>
2023-04-19 13:51:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,94 @@
+# **rn-style-props**
+
+This package is intended for React Native apps. It contains the constants and the functions to check the type of style props:
+[*layout*](https://reactnative.dev/docs/layout-props),
+[*transform*](#),
+[*shadow*](https://reactnative.dev/docs/shadow-props),
+[*view*](https://reactnative.dev/docs/view-style-props),
+[*text*](https://reactnative.dev/docs/text-style-props) or
+[*image*](https://reactnative.dev/docs/image-style-props).
+
+### **Constans**
+- `layoutStyleProps` is an array of all style props of *layout* type
+- `transformStyleProps` is an array of all style props of *transform* type
+- `shadowStyleProps` is an array of all style props of *shadow* type
+- `textStyleProps` is an array of all style props of *text* type
+- `imageStyleProps` is an array of all style props of *image* type
+
+### **Functions**
+- `isLayoutStyleProp`    
+  It returns `true` if a style prop is a *layout* type. The function parameter is the prop name string to check.
+- `isTransformStyleProp`    
+  It returns `true` if a style prop is a *transform* type. The function parameter is the prop name string to check.
+- `isShadowStyleProp`    
+  It returns `true` if a style prop is a *shadow* type. The function parameter is the prop name string to check.
+- `isViewStyleProp`    
+  It returns `true` if a style prop is a *view* type. The function parameter is the prop name string to check.
+- `isTextStyleProp`    
+  It returns `true` if a style prop is a *text* type. The function parameter is the prop name string to check.
+- `isImageStyleProp`    
+  It returns `true` if a style prop is a *image* type. The function parameter is the prop name string to check.   
+       
+- `isViewStyleValidProp`   
+  It returns `true` if a style prop is valid for `View` component, that is if its type is *layout*, *transform*, 
+  *shadow* or *view*. The function parameter is the prop name string to check.
+- `isTextStyleValidProp`    
+  It returns `true` if a style prop is valid for `Text` component, that is if it is valid for `View` component or
+  its type is *text*. The function parameter is the prop name string to check.
+- `isImageStyleValidProp`    
+  It returns `true` if a style prop is valid for `Image` component, that is if it is valid for `View` component or
+  its type is *image*. The function parameter is the prop name string to check.    
+         
+- `extractTextStyle`    
+  If you have an object containing a set of style props for a `Text` component, this function will separate all style
+  props whose *text* type. This function returns an object which has two properties:
+  + `view` is an object containing all style props which are valid for `View` component.
+  + `text` is an object containing all style props whose *text* type.  
+     
+  This function throws an error if there is a not valid style prop for a `Text` component.
+     
+  **Usage:**    
+  Suppose you create  a custom component:
+  ```
+  import React from 'react';
+  import {Text, View} from 'react-native';
+  import shallowCompare from 'react-addons-shallow-compare';
+  import Decorator from './Decorator';
+
+  class TextCustom extends React.Component {
+    #style = null;
+
+    constructor(props) {
+        super(props);
+        this.#style = extractTextStyle(props.style);
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (!Object.is(this.props.style, nextProps.style)) {
+            this.#style = extractTextStyle(nextProps.style);
+        }
+        return shallowCompare(this, nextProps, nextState);
+    }
+
+    render() {
+        return (
+            <View style={this.#style.view}>
+                <Decorator />
+                <Text style={this.#style.text}>{this.props.children}</Text>
+            </View>
+        );
+    }
+  }
+  ```  
+  In the above example, you just need define one prop for style. You don't need two props:
+  the first one is for container `View` and another one is for contained `Text`.
+
+  **Parameters:**
+  + `style` is an object containing the style props to be extracted.
+  + `paddingForText`: if it's `true` then all `padding` props will assigned to `text` property in
+    the returned object. It may be useful if the contained component is `TextInput`. This parameter
+    has default value, that is `false`.
+
+- `extractImageStyle`    
+  It's like `extractTextStyle` but for `Image` component. The properties of the returned object are
+  `view` and `image`.
---
+++ index.js
@@ -0,0 +1,248 @@
+/**
+ * rn-style-props
+ * https://github.com/atmulyana/rn-style-props
+ *
+ * @format
+ */
+import {StyleSheet} from 'react-native';
+
+const layoutStyle = {
+    alignContent: true,
+    alignItems: true,
+    alignSelf: true,
+    aspectRatio: true,
+    borderBottomWidth: true,
+    borderEndWidth: true,
+    borderLeftWidth: true,
+    borderRightWidth: true,
+    borderStartWidth: true,
+    borderTopWidth: true,
+    borderWidth: true,
+    bottom: true,
+    columnGap: true,
+    direction: true,
+    display: true,
+    end: true,
+    flex: true,
+    flexBasis: true,
+    flexGrow: true,
+    flexShrink: true,
+    gap: true,
+    height: true,
+    inset: true,
+    insetBlock: true,
+    insetBlockEnd: true,
+    insetBlockStart: true,
+    insetInline: true,
+    insetInlineEnd: true,
+    insetInlineStart: true,
+    left: true,
+    margin: true,
+    marginBlock: true,
+    marginBlockEnd: true,
+    marginBlockStart: true,
+    marginBottom: true,
+    marginEnd: true,
+    marginHorizontal: true,
+    marginInline: true,
+    marginInlineEnd: true,
+    marginInlineStart: true,
+    marginLeft: true,
+    marginRight: true,
+    marginStart: true,
+    marginTop: true,
+    marginVertical: true,
+    maxHeight: true,
+    maxWidth: true,
+    minHeight: true,
+    minWidth: true,
+    overflow: true,
+    padding: true,
+    paddingBlock: true,
+    paddingBlockEnd: true,
+    paddingBlockStart: true,
+    paddingBottom: true,
+    paddingEnd: true,
+    paddingHorizontal: true,
+    paddingInline: true,
+    paddingInlineEnd: true,
+    paddingInlineStart: true,
+    paddingLeft: true,
+    paddingRight: true,
+    paddingStart: true,
+    paddingTop: true,
+    paddingVertical: true,
+    position: true,
+    right: true,
+    rowGap: true,
+    start: true,
+    top: true,
+    width: true,
+    flexDirection: true,
+    flexWrap: true,
+    justifyContent: true,
+    zIndex: true,
+};
+
+const shadowStyle = {
+    shadowColor: true,
+    shadowOffset: true,
+    shadowOpacity: true,
+    shadowRadius: true,
+};
+
+const transformStyle = {
+    rotation: true,
+    scaleX: true,
+    scaleY: true,
+    transform: true,
+    transformMatrix: true,
+    translateX: true,
+    translateY: true,
+};
+
+const viewStyle = {
+    backfaceVisibility: true,
+    backgroundColor: true,
+    borderColor: true,
+    borderCurve: true,
+    borderBottomColor: true,
+    borderEndColor: true,
+    borderLeftColor: true,
+    borderRightColor: true,
+    borderStartColor: true,
+    borderTopColor: true,
+    borderBlockColor: true,
+    borderBlockEndColor: true,
+    borderBlockStartColor: true,
+    borderRadius: true,
+    borderBottomEndRadius: true,
+    borderBottomLeftRadius: true,
+    borderBottomRightRadius: true,
+    borderBottomStartRadius: true,
+    borderEndEndRadius: true,
+    borderEndStartRadius: true,
+    borderStartEndRadius: true,
+    borderStartStartRadius: true,
+    borderTopEndRadius: true,
+    borderTopLeftRadius: true,
+    borderTopRightRadius: true,
+    borderTopStartRadius: true,
+    borderStyle: true,
+    borderWidth: true,
+    borderBottomWidth: true,
+    borderEndWidth: true,
+    borderLeftWidth: true,
+    borderRightWidth: true,
+    borderStartWidth: true,
+    borderTopWidth: true,
+    elevation: true,
+    opacity: true,
+    pointerEvents: true,
+};
+
+const textStyle = {
+    color: true,
+    fontFamily: true,
+    fontSize: true,
+    fontStyle: true,
+    fontWeight: true,
+    fontVariant: true,
+    textShadowOffset: true,
+    textShadowRadius: true,
+    textShadowColor: true,
+    letterSpacing: true,
+    lineHeight: true,
+    textAlign: true,
+    textAlignVertical: true,
+    includeFontPadding: true,
+    textDecorationLine: true,
+    textDecorationStyle: true,
+    textDecorationColor: true,
+    textTransform: true,
+    userSelect: true,
+    verticalAlign: true,
+    writingDirection: true,
+};
+
+const imageStyle = {
+    objectFit: true,
+    overlayColor: true,
+    resizeMode: true,
+    tintColor: true,
+};
+
+
+export const layoutStyleProps = Object.keys(layoutStyle);
+export const transformStyleProps = Object.keys(transformStyle);
+export const shadowStyleProps = Object.keys(shadowStyle);
+export const viewStyleProps = Object.keys(viewStyle);
+export const textStyleProps = Object.keys(textStyle);
+export const imageStyleProps = Object.keys(imageStyle);
+
+export const isLayoutStyleProp = propName => layoutStyle[propName];
+export const isTransformStyleProp = propName => transformStyle[propName];
+export const isShadowStyleProp = propName => shadowStyle[propName];
+export const isViewStyleProp = propName => viewStyle[propName];
+export const isTextStyleProp = propName => textStyle[propName];
+export const isImageStyleProp = propName => imageStyle[propName];
+
+export const isViewStyleValidProp = propName =>
+    isLayoutStyleProp(propName) || isTransformStyleProp(propName) || isShadowStyleProp(propName) || isViewStyleProp(propName);
+export const isTextStyleValidProp = propName => isViewStyleValidProp(propName) || isTextStyleProp(propName);
+export const isImageStyleValidProp = propName => isViewStyleValidProp(propName) || isImageStyleProp(propName);
+
+export const extractTextStyle = (style, paddingForText = false, attrName='style') => {
+    const textStyle = {}, viewStyle = {};
+    style = StyleSheet.flatten(style);
+    for (let propName in style) {
+        if (!isTextStyleValidProp(propName))
+            throw `${propName} is not valid property for ${attrName}`;
+        if (isTextStyleProp(propName) || paddingForText && propName.indexOf('padding') > -1) {
+            textStyle[propName] = style[propName];
+        }
+        else {
+            viewStyle[propName] = style[propName];
+        }
+    }
+    return {view: viewStyle, text: textStyle};
+};
+
+export const extractImageStyle = (style, attrName='style') => {
+    const imageStyle = {}, viewStyle = {};
+    style = StyleSheet.flatten(style);
+    for (let propName in style) {
+        if (!isImageStyleValidProp(propName))
+            throw `${propName} is not valid property for ${attrName}`;
+        if (isImageStyleProp(propName)) {
+            imageStyle[propName] = style[propName];
+        }
+        else {
+            viewStyle[propName] = style[propName];
+        }
+    }
+    return {view: viewStyle, image: imageStyle};
+};
+
+export default {
+    layoutStyleProps,
+    transformStyleProps,
+    shadowStyleProps,
+    viewStyleProps,
+    textStyleProps,
+    imageStyleProps,
+    
+    isLayoutStyleProp,
+    isTransformStyleProp,
+    isShadowStyleProp,
+    isViewStyleProp,
+    isTextStyleProp,
+    isImageStyleProp,
+    
+    isViewStyleValidProp,
+    isTextStyleValidProp,
+    isImageStyleValidProp,
+
+    extractTextStyle,
+    extractImageStyle,
+};
