7672ce1e03ebf7595206edec72fc683846aacc85
AT Mulyana <atmulyana@yahoo.com>
2023-06-09 14:59:04 +0700

    fixes declaration file

--- index.d.ts
+++ index.d.ts
@@ -2,40 +2,45 @@
  * https://github.com/atmulyana/rn-style-props
  * @format
  */
+//import type {ImageStyle, TextStyle, ViewStyle} from 'react-native/Libraries/StyleSheet/StyleSheetTypes';
 type Arr = ReadonlyArray<string>;
 type Obj = Readonly<{[prop: string]: any}>;
 type StyleProps = null
-| undefined
-| Obj
-| false
-| ''
-| ReadonlyArray<StyleProps>;
+    | undefined
+    | Obj
+    | false
+    | ''
+    | ReadonlyArray<StyleProps>;
 
-declare export const layoutStyleProps: Arr;
-declare export const transformStyleProps: Arr;
-declare export const shadowStyleProps: Arr;
-declare export const viewStyleProps: Arr;
-declare export const textStyleProps: Arr;
-declare export const imageStyleProps: Arr;
+export const layoutStyleProps: Arr;
+export const transformStyleProps: Arr;
+export const shadowStyleProps: Arr;
+export const viewStyleProps: Arr;
+export const textStyleProps: Arr;
+export const imageStyleProps: Arr;
 
-declare export function isLayoutStyleProp(propName: string): boolean;
-declare export function isTransformStyleProp(propName: string): boolean;
-declare export function isShadowStyleProp(propName: string): boolean;
-declare export function isViewStyleProp(propName: string): boolean;
-declare export function isTextStyleProp(propName: string): boolean;
-declare export function isImageStyleProp(propName: string): boolean;
+export function isLayoutStyleProp(propName: string): boolean;
+export function isTransformStyleProp(propName: string): boolean;
+export function isShadowStyleProp(propName: string): boolean;
+export function isViewStyleProp(propName: string): boolean;
+export function isTextStyleProp(propName: string): boolean;
+export function isImageStyleProp(propName: string): boolean;
 
-declare export function isViewStyleValidProp(propName: string): boolean;
-declare export function isTextStyleValidProp(propName: string): boolean;
-declare export function isImageStyleValidProp(propName: string): boolean;
+export function isViewStyleValidProp(propName: string): boolean;
+export function isTextStyleValidProp(propName: string): boolean;
+export function isImageStyleValidProp(propName: string): boolean;
 
-declare export function extractTextStyle(style: StyleProps, paddingForText?: boolean, attrName?: string): {
+export function extractTextStyle(style: StyleProps, paddingForText?: boolean, attrName?: string): {
+    // view: Readonly<ViewStyle>,
+    // text: Readonly<TextStyle>,
     view: Obj,
     text: Obj,
     unknown: Obj,
 };
 
-declare export function extractImageStyle(style: StyleProps, attrName?: string): {
+export function extractImageStyle(style: StyleProps, attrName?: string): {
+    // view: Readonly<ViewStyle>,
+    // image: Readonly<ImageStyle>,
     view: Obj,
     image: Obj,
     unknown: Obj,
